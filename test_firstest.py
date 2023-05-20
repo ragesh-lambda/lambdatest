b2721890a1dea4a68317c19a05ba5d9a412490be
ritamganguli <35348707+ritamganguli@users.noreply.github.com>
2023-05-20 20:12:16 +0530

    he he

--- test_firstest.py
+++ test_firstest.py
@@ -28,32 +28,33 @@ capability_1= {
     }
 }
 
-driver_1 = webdriver.Remote(
-    command_executor=url,
-    desired_capabilities=capability_1
-)
-driver_1.maximize_window()
-driver_1.get("http://whatismyscreenresolution.net/")
-time.sleep(10)
-driver_1.get("https://uat-dv.docveda.in/")
-driver_1.find_element("xpath", "//button[contains(text(), 'Log In')]").click()
-driver_1.find_element("id","outlined-basic").send_keys("SRP1")
-driver_1.find_element("name","userName").send_keys("SRP123")
-driver_1.find_element("name","password").send_keys("Admin@12345")
-# driver_1.find_element("type","checkbox").click()
-driver_1.find_element("xpath","/html/body/div[3]/div[3]/div/div[2]/div/div[2]/div/div/form/div[5]/button").click()
-time.sleep(8)
-driver_1.find_element("xpath","/html/body/div[1]/div/div[5]/div[3]/div/div[3]/div/div[2]/div/div/div[3]/div[1]/div[1]").click()
+def test_testmethoud():
+    driver_1 = webdriver.Remote(
+        command_executor=url,
+        desired_capabilities=capability_1
+    )
+    driver_1.maximize_window()
+    driver_1.get("http://whatismyscreenresolution.net/")
+    time.sleep(10)
+    driver_1.get("https://uat-dv.docveda.in/")
+    driver_1.find_element("xpath", "//button[contains(text(), 'Log In')]").click()
+    driver_1.find_element("id","outlined-basic").send_keys("SRP1")
+    driver_1.find_element("name","userName").send_keys("SRP123")
+    driver_1.find_element("name","password").send_keys("Admin@12345")
+    # driver_1.find_element("type","checkbox").click()
+    driver_1.find_element("xpath","/html/body/div[3]/div[3]/div/div[2]/div/div[2]/div/div/form/div[5]/button").click()
+    time.sleep(8)
+    driver_1.find_element("xpath","/html/body/div[1]/div/div[5]/div[3]/div/div[3]/div/div[2]/div/div/div[3]/div[1]/div[1]").click()
 
-# driver_1.find_element("id","email").send_keys(username)
-# driver_1.find_element("id","password").send_keys(passs)
-#driver.find_element("id","login-button").click()
-# elem = driver_1.find_element("id","btnHover")
-# elem.send_keys("https://accounts.lambdatest.com/login")
-# elem.submit()
-# driver_1.find_element("id","login-button").click()
-print("Printing title of current page :"+driver_1.title)
-driver_1.execute_script("lambda-status=passed")
-print("Requesting to mark test : pass")
-driver_1.quit()
+    # driver_1.find_element("id","email").send_keys(username)
+    # driver_1.find_element("id","password").send_keys(passs)
+    #driver.find_element("id","login-button").click()
+    # elem = driver_1.find_element("id","btnHover")
+    # elem.send_keys("https://accounts.lambdatest.com/login")
+    # elem.submit()
+    # driver_1.find_element("id","login-button").click()
+    print("Printing title of current page :"+driver_1.title)
+    driver_1.execute_script("lambda-status=passed")
+    print("Requesting to mark test : pass")
+    driver_1.quit()
 
